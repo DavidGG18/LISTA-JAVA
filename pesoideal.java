7978bc02f0d8b55a210fb955a2f11278e93a6c2d
David Gabriel Fonseca <116889386+DavidGG18@users.noreply.github.com>
2023-04-04 08:34:59 -0300

    Update pesoideal.java

--- pesoideal.java
+++ pesoideal.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 public class pesoideal  {
 
-
+    double pesoH;
+    double pesoM;
+    char genero;
     public static void main(String[]args) throws  Exception{
 Scanner input = new Scanner(System.in);
 
 System.out.println(" Digite sua altura ");
-double altura = input.nextInt();
+float altura = input.nextFloat();
 
-System.out.print(" Digite seu genro  F ou M");
-//char genero = input.nextChar();
-char genero;
- genero = input.next().charAt(0);
+System.out.println(" Digite seu gêne1ro  F ou M");
+char genero = input.next().charAt(0); // Ler caracteres letras 
+
+
 double pesoM = ( 72.7 * altura ) - 58;
 double pesoF = ( 62.1 * altura ) - 44.7;
+
+
 input.close();
-if (genero=='M') {
+if ( genero == 'M') {
 System.out.print(" Seu peso ideal de acordo com sua altura é = " + pesoM);
 }
 
-if (genero=='F') {
+else if ( genero == 'F') {
 System.out.print("  Seu peso ideal de acordo com sua altura é = " + pesoF);
+} else  {
+    System.out.println(" gênero invalido ");
 }
 
     }
